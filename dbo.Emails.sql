0f20e16f4dd2637196efaf2e9f007ce57f5e4715
ilay00 <agidelio@mail.ru>
2019-07-22 23:43:08 +0400

    2-2 23-07

---
+++ dbo.Emails.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Emails] (
+    [Id]     INT            NOT NULL,
+    [Email] NVARCHAR (MAX) NOT NULL,
+    [Name]   NVARCHAR (MAX) NOT NULL,
+    CONSTRAINT [PK_Email_Id] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
