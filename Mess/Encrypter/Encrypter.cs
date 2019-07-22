8861d43419e4b7f95073777df46a5add89f8b8a6
ilay00 <agidelio@mail.ru>
2019-07-22 23:48:33 +0400

    2307

---
+++ Mess/Encrypter/Encrypter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncrypterDLL
+{
+    public static class Encrypter
+    {
+        public static string Encrypt (string str, int key = 42) => new string(str.Select(c => (char)(c + key)).ToArray());
+        public static string Deencrypt (string str, int key = 42) => new string(str.Select(c => (char)(c - key)).ToArray());
+    }
+}
